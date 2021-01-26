eab4eb30d0460eb9296356d1c5acb2b868843e68
clement000 <=>
2021-01-26 13:10:09 +0100

    added a split mechanic for the green Blobs

--- Assets/Scripts/GreenBlob/GreenBlob_Behaviour.cs
+++ Assets/Scripts/GreenBlob/GreenBlob_Behaviour.cs
@@ -7,38 +7,50 @@ public class GreenBlob_Behaviour : MonoBehaviour
     public float averageIdleTime = 1f;
     public float minJumpDistance = 1f;
     public float maxJumpDistance = 1f;
+    public float averageSplitTime = 30f;
+    public float minimalDensityToSplit = 15f;
     GridSystem grid;
+    
 
     private float jumpDuration = 0.5f;
-    private float timer = 0f;
+    private float jumpTimer = 0f;
+    private float splitTimer = 0f;
     private bool isIdle = true;
+    private bool canSplit = false;
     private float idleTime;
     private float lastIdleX;
     private float lastIdleY;
+    private float splitTime;
 
     private Vector2 jumpVector = Vector2.zero;
     private float theta;
     private float distance;
 
+    private bool blobWasInit = false;
+
+    private Rigidbody2D GreenBlob;
+
     // Start is called before the first frame update
     void Start()
     {
+        jumpTimer = jumpDuration;
+        GreenBlob = GetComponent<Rigidbody2D>();
         Transform transform = gameObject.GetComponent<Transform>();
         idleTime = Random.Range(averageIdleTime - averageIdleTime / 2, averageIdleTime + averageIdleTime / 2);
         lastIdleX = transform.position.x;
         lastIdleY = transform.position.y;
-        
-        
+        splitTime = Random.Range(1.5f, 1) * averageSplitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        grid = GameObject.Find("GameSystem").GetComponent<GameSystem>().greenBlobHeatmap;
         if (isIdle)
         {
-            if (timer > idleTime)//it is time to jump !
+            if (jumpTimer > idleTime)//it is time to jump !
             {
-                timer = 0f;
+                jumpTimer = 0f;
                 isIdle = false;
                 // Initialize the Jump                
                 float privileged = PrivilegedDirection();
@@ -49,31 +61,60 @@ public class GreenBlob_Behaviour : MonoBehaviour
                 jumpVector.y = Mathf.Sin(theta) * distance;
                 lastIdleX = transform.position.x;
                 lastIdleY = transform.position.y;
-                grid = GameObject.Find("GameSystem").GetComponent<GameSystem>().greenBlobHeatmap;
-                grid.AddBlobToHeatMap(transform.position);
+                
             }
         }
         else
         {
-            if (timer > jumpDuration)//it is time to stop jumping !
+            if (jumpTimer > jumpDuration)//it is time to stop jumping !
             {
-                timer = 0f;
+                jumpTimer = 0f;
                 isIdle = true;
                 idleTime = Random.Range(averageIdleTime - averageIdleTime / 2, averageIdleTime + averageIdleTime / 2);
-                grid = GameObject.Find("GameSystem").GetComponent<GameSystem>().greenBlobHeatmap;
-                grid.RemoveBlobFromHeatMap(new Vector3(lastIdleX,lastIdleY));
+                if (blobWasInit)
+                {
+                    grid.RemoveBlobFromHeatMap(new Vector3(lastIdleX, lastIdleY));
+                }
+                grid.AddBlobToHeatMap(transform.position);
+                Move(new Vector2(0, 0));
+                blobWasInit = true;
             }
             else
             {
-                Move(jumpVector * (Time.deltaTime / jumpDuration));
+                Move(jumpVector);
             }
         }
-        timer += Time.deltaTime;
+        int xBlob, yBlob;
+        grid.GetXY(transform.position, out xBlob, out yBlob);
+        if (grid.value(xBlob, yBlob) < 8)
+        {
+            if (canSplit)
+            { 
+                Split();
+                canSplit = false;
+                splitTimer = 0f;
+                splitTime = Random.Range(1.5f, 1) * averageSplitTime;
+            }
+            else
+            {
+                if (splitTimer > splitTime)
+                {
+                    canSplit = true;
+                }
+            }
+            splitTimer += Time.deltaTime;
+        }
+        jumpTimer += Time.deltaTime;
     }
     void Move(Vector2 translation2)
     {
-        Vector3 translation3 = translation2;
-        transform.Translate(translation3);
+        Vector2 velocity = translation2;
+        GreenBlob.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpVector = -1 * jumpVector;
     }
 
     private float PrivilegedDirection()
@@ -131,6 +172,11 @@ public class GreenBlob_Behaviour : MonoBehaviour
         s = Mathf.Sqrt((-2.0f * Mathf.Log(s)) / s);
 
         return v1 * s;
+    }
+    private void Split()
+    {
+        Debug.Log("split !");
+        Instantiate(GreenBlob);
     }
 }
 
