83a73f247a8bd88e5068d32aa362f9c34a190939
eduss <e.dussy-lachaud@wanadoo.fr>
2021-01-29 11:56:42 +0100

    Main menu working

---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MainMenuButton();
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+    }
+
+    public void PlayNowButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+
+}
